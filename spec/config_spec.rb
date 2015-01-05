3253047625953540f3be90481b88494f874e7e00
Paul Ballesty <paulballesty@gmail.com>
2015-01-05 19:14:15 -0300

    Makes the base_url configurable

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -6,18 +6,19 @@ describe "RoomoramaApi" do
     it "instanciate config object" do
       expect( roomorama_config ).to be_instance_of( RoomoramaApi::Config )
     end
- 
-    describe "object attributes" do 
+
+    describe "object attributes" do
       subject { RoomoramaApi::Config.new }
       it { is_expected.to respond_to(:client_id) }
       it { is_expected.to respond_to(:client_secret) }
       it { is_expected.to respond_to(:token) }
+      it { is_expected.to respond_to(:base_url) }
     end
 
     describe "#base_url" do
-      it "responds with base_url" do
+      it "responds with the default base_url" do
         expect( roomorama_config ).to respond_to(:base_url)
-        expect( roomorama_config.base_url ).to eql( "https://api.staging.roomorama.com" )
+        expect( roomorama_config.base_url ).to eql( "https://api.roomorama.com" )
       end
     end
   end
